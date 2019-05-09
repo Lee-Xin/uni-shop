1257a61430a345f0fce0c9b188166aab739eea32
Lee-Xin <504560093@qq.com>
2019-05-09 11:04:54 +0800

    优化获取token代码块

--- common/lib/commonFunc.js
+++ common/lib/commonFunc.js
@@ -4,13 +4,12 @@ exports.getToken = () => {
 	// 优先从store取token
 	if(Vue.prototype.$store && Vue.prototype.$store.getters.ut){
 		return Vue.prototype.$store.getters.ut
-	} else {
-		try{
-			let token = uni.getStorageSync('token')
-			return token
-		}catch(e){
-			return null
-		}
-
+	} 
+	// 其次从缓存中取token
+	try{
+		let token = uni.getStorageSync('token')
+		return token
+	}catch(e){
+		return null
 	}
 }
